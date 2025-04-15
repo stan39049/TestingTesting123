32860b0c026ae4c1be6baec224bb386957e77432
stan39049 <stan39049@gmail.com>
2025-04-15 11:19:50 -0400

    Completed TODO 1

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -16,7 +16,8 @@ class UniqueCollectionTest {
     // TODO 1: Write a test to ensure items can be added to the collection
     @Test
     fun addAnItem() {
-
+        collection.addItem(Item("item1"))
+        assert(collection.size() == 1) { "Item not added" }
     }
 
     // TODO 2: Write a test to ensure that only unique items can be added to the collection
